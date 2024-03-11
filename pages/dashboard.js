0a9a8c0e8129a357cd42ded905728e3439d2d97e
Tien-A <tienbluejay@gmail.com>
2024-03-11 15:29:47 +0700

    handle check Login

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../src/ui-components/layout";
 import Dashboard from "../src/Pages/dashboard";
+import { useRouter } from "next/router";
+import { isLoggedIn } from "./api/auth/login";
 
-const AdminPage = () => (
-  <Layout>
-    <Dashboard />
-  </Layout>
-);
+const AdminPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+};
 
 export default AdminPage;
